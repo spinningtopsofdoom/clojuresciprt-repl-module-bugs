8b2a962170f8f5330ce8c456b864b1aa674c9962
Peter Schuck <peter@bendyworks.com>
2017-07-20 11:16:21 -0500

    Update for latest ClojureScript

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    ["maven-central" {:url "https://repo1.maven.org/maven2"}]]
   :dependencies
   [[org.clojure/clojure "1.8.0"]
-   [org.clojure/clojurescript "1.9.815"]
+   [org.clojure/clojurescript "1.9.820"]
    [com.andrewmcveigh/cljs-time "0.5.0"]]
   :source-paths
   ["src/main"])
